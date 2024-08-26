92a47d10b69fd3a40291e609fa2ecc1a59bea00c
Christopher Ritchey <155682+Aperion@users.noreply.github.com>
2024-08-26 14:06:23 -0700

    Add Dependency properties

--- WpfApp1/Child.xaml.cs
+++ WpfApp1/Child.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp1;
 
@@ -8,6 +10,46 @@ public partial class Child : UserControl
     {
         InitializeComponent();
     }
-    public int FanSpeed { get; set; }
-    public double Temperature { get; set; }
+
+    public double FanSpeed
+    {
+        get => (double)GetValue(FanSpeedProperty);
+        set => SetValue(FanSpeedProperty, value);
+    }
+
+    public static readonly DependencyProperty FanSpeedProperty =
+        DependencyProperty.Register(nameof(FanSpeed),
+            typeof(double),
+            typeof(Child),
+            new PropertyMetadata((o, args) =>
+            {
+                if (o is null)
+                {
+                    Console.WriteLine("trying to set FanSpeed to null");
+                    return;
+                }
+
+                ((o as Child)!).FanSpeed = (double)args.NewValue;
+            }));
+
+    public double Temperature
+    {
+        get => (double)GetValue(TemperatureProperty);
+        set => SetValue(TemperatureProperty, value);
+    }
+
+    public static readonly DependencyProperty TemperatureProperty =
+        DependencyProperty.Register(nameof(Temperature),
+            typeof(double),
+            typeof(Child),
+            new PropertyMetadata((o, args) =>
+            {
+                if (o is null)
+                {
+                    Console.WriteLine("trying to set Temperature to null");
+                    return;
+                }
+
+                ((o as Child)!).Temperature = (double)args.NewValue;
+            }));
 }
--- WpfApp1/Parent.xaml.cs
+++ WpfApp1/Parent.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp1;
 
@@ -8,4 +10,23 @@ public partial class Parent : UserControl
     {
         InitializeComponent();
     }
+    
+    public DataInfo Data { get; set; }
+    
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register(nameof(Data),
+            typeof(DataInfo),
+            typeof(Parent),
+            new PropertyMetadata(new DataInfo(), (o, args) =>
+            {
+                if(o is null)
+                {
+                    Console.WriteLine("trying to set Data to null");
+                    return;
+                }
+                
+                ((o as Parent)!).Data = (DataInfo)args.NewValue;
+            }));
+    
+    
 }
